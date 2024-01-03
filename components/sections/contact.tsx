f3b409bf49a303242163997a198485267a66f922
Yusuf <yusuf2002kh@gmail.com>
2024-01-03 12:46:53 +0500

    footer

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -16,8 +16,8 @@ const Contact = () => {
 					/>
 					<textarea
 						className='block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-						cols='30'
-						rows='10'
+						cols={30}
+						rows={8}
 						placeholder='Message'
 					></textarea>
 				</form>
